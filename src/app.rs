dc748a403ab4e4a81d41a40779bee85bd9f80703
tokcum <tobias.mucke@gmail.com>
2023-12-19 21:39:07 +0100

    add: start page.
    Signed-off-by: tokcum <tobias.mucke@gmail.com>

---
+++ src/app.rs
@@ -0,0 +1,10 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+pub fn app() -> Html {
+    html! {
+        <main>
+            <img class="logo" src="/assets/logo/transparent_logo_black.png" alt="Kodiak Logo" />
+        </main>
+    }
+}
